4a80fd28007a303d7e84d2665458e957c2a9a980
lfg-max <nra.deeplearning02@gmail.com>
2026-01-31 07:27:41 +0530

    Delete week_2_mls directory

--- week_2_mls/config.py
+++
@@ -1,5 +0,0 @@
-# (nra_upd_02)
-
-# Common configurations and constants.
-
-HF_REPO_ID = "Nra/Machine-Failure-Prediction" # name is case sensitive
--- week_2_mls/deployment/config.py
+++
@@ -1,5 +0,0 @@
-# (nra_upd_02)
-# Common configurations and constants.  this has to be created separately.
-# as the week_2_mls folder doesn not exist at HF
-
-HF_REPO_ID = "Nra/Machine-Failure-Prediction" # name is case sensitive
