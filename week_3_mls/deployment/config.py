30a406895bb6b5eff8536f9594989004f24b1485
{github_username} <{github_email}>
2026-03-22 07:39:47 +0000

    commit from MLOps Notebook

---
+++ week_3_mls/deployment/config.py
@@ -0,0 +1,5 @@
+# (nra_upd_02)
+# Common configurations and constants.  this has to be created separately
+# as the week_3_mls folder doesn not exist at HF
+
+HF_REPO_ID = "Nra/Machine-Failure-Prediction" # name is case sensitive
